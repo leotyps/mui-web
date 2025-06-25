f3775a27d025a00419259bac973fb2d198527fdf
leotyps <113692736+leotyps@users.noreply.github.com>
2025-06-25 15:36:57 +0700

    Create theme.ts

---
+++ src/theme.ts
@@ -0,0 +1,27 @@
+import { createTheme } from '@mui/material/styles';
+import { red } from '@mui/material/colors';
+
+// Buat objek tema kustom
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#556cd6',
+    },
+    secondary: {
+      main: '#19857b',
+    },
+    error: {
+      main: red.A400,
+    },
+  },
+  typography: {
+    fontFamily: [
+      'Roboto',
+      '"Helvetica Neue"',
+      'Arial',
+      'sans-serif'
+    ].join(','),
+  },
+});
+
+export default theme;
